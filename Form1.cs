15ac0ec61d8bc29ee14f22d4e3a5bd1b0ec5084f
Lenovo-User <Lenovo-User@DESKTOP-TIU29D7>
2024-01-24 13:42:21 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C___CIRCLE_AREA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a;
+            double r;
+            const double pi = 3.14;
+
+                if (double.TryParse(textBox1.Text, out r))
+            {
+                a = pi * Math.Pow(r,2);
+                textBox2.Text= a.ToString();
+
+            }
+                else
+            {
+                MessageBox.Show("ENTER A  VALID NUMERIC VALUE ");
+
+            }
+        }
+    }
+}
